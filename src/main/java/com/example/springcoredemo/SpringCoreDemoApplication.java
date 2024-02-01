6d87fe3f85958e604c40ce11d87d57c6763b8ffa
baderbenlhachemi <[baderbenlhachemi@gmail.com])>
2024-02-02 00:22:02 +0100

    Component scanning

--- src/main/java/com/example/springcoredemo/SpringCoreDemoApplication.java
+++ src/main/java/com/example/springcoredemo/SpringCoreDemoApplication.java
@@ -1,17 +1,22 @@
 package com.example.springcoredemo;
 
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication; // Behind the scenes, creates an application context and registers all the beans & starts the embedded server.
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 // @SpringBootApplication is a convenience annotation that adds all of the following:
 // @Configuration: Tags the class as a source of bean definitions for the application context.
 // @EnableAutoConfiguration: Tells Spring Boot to start adding beans based on classpath settings, other beans, and various property settings.
 // @ComponentScan: Tells Spring to look for other components, configurations, and services in the com/example package, letting it find the controllers.
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = {
+        "com.example.springcoredemo",
+        "com.example.util"
+    }
+)
 public class SpringCoreDemoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringCoreDemoApplication.class, args);
+        SpringApplication.run(SpringCoreDemoApplication.class, args); // Bootstraps the application
     }
 
 }
