44f24221cf7a90be5c29662978cf7538e4ab79ff
baderbenlhachemi <[baderbenlhachemi@gmail.com])>
2024-02-01 16:13:17 +0100

    Constructor Injection

---
+++ src/main/java/com/example/springcoredemo/DemoController.java
@@ -0,0 +1,21 @@
+package com.example.springcoredemo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    private Coach myCoach; // Define a private field for the dependency
+
+    @Autowired // Tells Spring to inject the dependency. If we have one constructor, we can omit @Autowired
+    public DemoController(Coach coach) { // Constructor injection
+        this.myCoach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return myCoach.getDailyWorkout();
+    }
+}
