88be9de660d8cedea0392a4a659ed276cdc72e96
baderbenlhachemi <[baderbenlhachemi@gmail.com])>
2024-02-02 11:45:18 +0100

    Qualifier

--- src/main/java/com/example/springcoredemo/rest/DemoController.java
+++ src/main/java/com/example/springcoredemo/rest/DemoController.java
@@ -2,6 +2,7 @@ package com.example.springcoredemo.rest;
 
 import com.example.springcoredemo.common.Coach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,7 +12,7 @@ public class DemoController {
     private Coach myCoach; // Define a private field for the dependency
 
     @Autowired // Tells Spring to inject the dependency. If we have one constructor, we can omit @Autowired
-    public DemoController(Coach coach) { // Constructor injection, use it when we have required dependencies
+    public DemoController(@Qualifier("baseballCoach") Coach coach) { // Constructor injection, use it when we have required dependencies
         this.myCoach = coach;
     }
 
