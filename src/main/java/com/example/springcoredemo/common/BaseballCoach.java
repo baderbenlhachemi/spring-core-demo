43c3f3a762740d0a6dab375d39adf0d45e8ea457
baderbenlhachemi <[baderbenlhachemi@gmail.com])>
2024-02-02 15:40:42 +0100

    Bean lifecycle methods (hooks)

--- src/main/java/com/example/springcoredemo/common/BaseballCoach.java
+++ src/main/java/com/example/springcoredemo/common/BaseballCoach.java
@@ -1,16 +1,31 @@
 package com.example.springcoredemo.common;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BaseballCoach implements Coach {
 
     public BaseballCoach() {
         System.out.println("In constructor: " + this.getClass().getName());
     }
+
+    // define init method
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff: " + getClass().getSimpleName());
+    }
+
+    // define destroy method
+    @PreDestroy
+    public void doMyCleanupStuff() {
+        System.out.println("In doMyCleanupStuff: " + getClass().getSimpleName());
+    }
+
     @Override
     public String getDailyWorkout() {
         return "Spend 30 minutes on batting practice";
